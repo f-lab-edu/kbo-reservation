3cd8698111830cedab7999a2a0649dcf0b673863
LimJiNa <jn4624@gmail.com>
2025-03-01 23:22:29 +0900

    Initial commit

---
+++ src/main/java/com/kbo/KboReservationApplication.java
@@ -0,0 +1,13 @@
+package com.kbo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KboReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KboReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kbo/KboReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KboReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
