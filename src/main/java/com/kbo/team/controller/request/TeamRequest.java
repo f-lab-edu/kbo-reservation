e7f60de3191d8e65b3f9761ca545965119d496e0
LimJiNa <jn4624@gmail.com>
2025-03-10 03:00:41 +0900

    [#11] feat: dto 객체 record 객체로 변환

--- src/main/java/com/kbo/team/controller/request/TeamRequest.java
+++ src/main/java/com/kbo/team/controller/request/TeamRequest.java
@@ -1,14 +1,6 @@
 package com.kbo.team.controller.request;
 
-public class TeamRequest {
-	private String name;
-	private long stadiumId;
-
-	public String getName() {
-		return name;
-	}
-
-	public long getStadiumId() {
-		return stadiumId;
-	}
+public record TeamRequest(
+	String name, long stadiumId
+) {
 }
