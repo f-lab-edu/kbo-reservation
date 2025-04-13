e81db2c4fb48521dc2c2cc7edc6102794071dff4
LimJiNa <jn4624@gmail.com>
2025-04-13 17:45:54 +0900

    [#23] feat: uri 표현법 수정 및 repository 일관성 개선

--- src/main/java/com/kbo/seatblock/repository/SeatBlockRepository.java
+++ src/main/java/com/kbo/seatblock/repository/SeatBlockRepository.java
@@ -1,9 +1,11 @@
 package com.kbo.seatblock.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kbo.seatblock.entity.SeatBlock;
 
+@Repository
 public interface SeatBlockRepository extends JpaRepository<SeatBlock, Long> {
 	boolean existsByName(String name);
 }
--- src/main/java/com/kbo/team/repository/TeamRepository.java
+++ src/main/java/com/kbo/team/repository/TeamRepository.java
@@ -1,9 +1,11 @@
 package com.kbo.team.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.kbo.team.entity.Team;
 
+@Repository
 public interface TeamRepository extends JpaRepository<Team, Long> {
 	boolean existsByName(String name);
 }
